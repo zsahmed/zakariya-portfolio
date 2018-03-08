c3cd7efc0dbf25e7da12e254d381baeb73aa1891
Zakariya Ahmed <zahmed@credera.com>
2018-03-07 23:08:54 -0600

    conditionally showing projects on works page using @click and v-if

--- src/components/works/Works.vue
+++ src/components/works/Works.vue
@@ -1,14 +1,30 @@
 <template lang="html">
   <div class="works-container">
     <div class="landingPage"></div>
-    <app-opp-leads-project></app-opp-leads-project>
-    <app-same-day-service-project></app-same-day-service-project>
-    <app-easy-pay-project></app-easy-pay-project>
-    <app-vizient-project></app-vizient-project>
-    <app-gunpla-webscraper-project></app-gunpla-webscraper-project>
-    <app-spring-exception-project></app-spring-exception-project>
-    <app-rick-and-morty-project></app-rick-and-morty-project>
-    <app-romo-project></app-romo-project>
+    <hr>
+
+    <div class="toggle-container">
+      <div class="row">
+        <button class="btn btn-success" @click="showAll">All Projects</button>
+        <button class="btn btn-danger" @click="showProfessional">Professional</button>
+        <button class="btn btn-danger" @click="showPersonal">Personal</button>
+      </div>
+    </div>
+
+
+    <template v-if="professional">
+      <app-opp-leads-project></app-opp-leads-project>
+      <app-same-day-service-project></app-same-day-service-project>
+      <app-easy-pay-project></app-easy-pay-project>
+      <app-vizient-project></app-vizient-project>
+    </template>
+
+    <template v-if="personal">
+      <app-gunpla-webscraper-project></app-gunpla-webscraper-project>
+      <app-spring-exception-project></app-spring-exception-project>
+      <app-rick-and-morty-project></app-rick-and-morty-project>
+      <app-romo-project></app-romo-project>
+    </template>
 
   </div>
 </template>
@@ -25,6 +41,26 @@ import HASameDayService from './entries/HASameDayService.vue';
 import HAOppLeads from './entries/HAOppLeads.vue';
 
 export default {
+  data() {
+    return {
+      personal: true,
+      professional: true
+    }
+  },
+  methods: {
+    showPersonal() {
+      this.personal = true;
+      this.professional = false;
+    },
+    showProfessional() {
+      this.personal = false;
+      this.professional = true;
+    },
+    showAll() {
+      this.personal = true;
+      this.professional = true;
+    }
+  },
   components: {
     appRomoProject: TonyRomoJsProject,
     appRickAndMortyProject: RMAndroidProject,
@@ -48,6 +84,16 @@ export default {
     background-size: cover;
     width: 100%;
     height: 500px;
+  }
+
+  .toggle-container {
+    width: 90%;
+    margin: auto;
     margin-bottom: 50px;
+  }
+
+  hr {
+    margin: 0 0 40px 0;
+    border-top: 5px solid #34495e;
   }
 </style>
